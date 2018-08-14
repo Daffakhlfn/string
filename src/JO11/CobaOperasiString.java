a3db9c467a71bf578b693b6125fe47547bc661cc
Asus <Asus@DESKTOP-8UAA3ST>
2018-08-14 09:46:32 +0700

    Initial Commit

---
+++ src/JO11/CobaOperasiString.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JO11;
+
+/**
+ *
+ * @author Asus
+ */
+public class CobaOperasiString {
+    public static void main(String[] args){
+        String identitas = "Mohammad Daffa Khalfani Sujatmiko / X-RPL-3 / 23";
+        System.out.println("Identitas : "+ identitas);
+    }
+}
