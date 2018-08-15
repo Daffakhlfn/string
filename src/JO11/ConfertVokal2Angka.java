eaa5675ea07453367a620dc457ad4c4aa543167a
Asus <Asus@DESKTOP-8UAA3ST>
2018-08-15 18:14:06 +0700

    Initial Commit

---
+++ src/JO11/ConfertVokal2Angka.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JO11;
+
+/**
+ *
+ * @author Asus
+ */
+public class ConfertVokal2Angka {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConfert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    String identitas = "Mohammad Daffa Khalfani Sujatmiko / X-RPL-3 / 23";
+    
+    tampilJudul(identitas);
+    
+}
