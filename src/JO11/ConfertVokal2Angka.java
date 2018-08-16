78e10d742ac8635ff86cb1eb71014ce3c683e35f
Asus <Asus@DESKTOP-8UAA3ST>
2018-08-16 18:03:25 +0700

    Tambah Input

--- src/JO11/ConfertVokal2Angka.java
+++ src/JO11/ConfertVokal2Angka.java
@@ -5,20 +5,24 @@
  */
 package JO11;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
  */
 public class ConfertVokal2Angka {
+    Scanner scanner = new Scanner(System.in);
     private static void tampilJudul(String identitas){
         System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nConfert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
+    public static void main(String[] args){
     String identitas = "Mohammad Daffa Khalfani Sujatmiko / X-RPL-3 / 23";
     
     tampilJudul(identitas);
-    
+    }
     private static String tampilInput(){
         Scanner scanner = new Scanner(System.in);
         
@@ -30,4 +34,5 @@ public class ConfertVokal2Angka {
         
     }
     String kalimat = tampilInput();
+
 }
