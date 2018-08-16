cbf60fa648d280e08ccd694b4df4f69bd7f34a74
Asus <Asus@DESKTOP-8UAA3ST>
2018-08-16 21:25:54 +0700

    Tambah Input

--- src/JO11/ConfertVokal2Angka.java
+++ src/JO11/ConfertVokal2Angka.java
@@ -12,18 +12,14 @@ import java.util.Scanner;
  * @author Asus
  */
 public class ConfertVokal2Angka {
-    Scanner scanner = new Scanner(System.in);
-    private static void tampilJudul(String identitas){
+    private static void tampilJudul(String identitas)
+    {
         System.out.println("Identitas : "+ identitas);
         
         System.out.println("\nConfert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
-    public static void main(String[] args){
-    String identitas = "Mohammad Daffa Khalfani Sujatmiko / X-RPL-3 / 23";
-    
-    tampilJudul(identitas);
-    }
-    private static String tampilInput(){
+        private static String tampilInput()
+    {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Masukkan Kalimat: ");
@@ -31,32 +27,7 @@ public class ConfertVokal2Angka {
         System.out.println("Kalimat Asli : " + kalimat);
         
         return kalimat;
-        
+
     }
-    String kalimat = tampilInput();
-    private static String vocal2Angka(String kalimat){
-     char [][] arConvert =
-     {
-         {'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}
-     };
-     
-     kalimat = kalimat.toLowerCase();
-     for(int i=0; i<arConvert.length; i++)
-         kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-     
-     return kalimat;
-    }
-    String convert = vocal2Angka(kalimat);
-    private static void tampilPerKata(String kalimat, String convert){
-        String[] arrKal = kalimat.split(" ");
-        String[] arrCon = convert.split(" ");
-        
-        for(int i=0; i<arrKal.length; i++)
-            System.out.println(arrKal[i]+" => "+arrCon[i]);
-    }
-    tampilPerKata(kalimat, convert);
-    private static void tampilHasil(String convert){
-        System.out.println("Kalimat Alay Angka : " + convert);
-    }
-    tampilHasil(convert);
 }
+
